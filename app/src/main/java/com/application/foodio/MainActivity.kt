17fe01763817deb5c25249d196cb84df91e13157
msalmancode <msalman.programmer@gmail.com>
2023-07-30 19:30:01 +0500

    first commit

---
+++ app/src/main/java/com/application/foodio/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.application.foodio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //Testing fist commit
+}
