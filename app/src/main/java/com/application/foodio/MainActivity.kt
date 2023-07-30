124849efdcf44ae28b072be6495076573e7ede93
msalmancode <msalman.programmer@gmail.com>
2023-07-30 22:18:04 +0500

    added bottom navigation

--- app/src/main/java/com/application/foodio/MainActivity.kt
+++ app/src/main/java/com/application/foodio/MainActivity.kt
@@ -2,11 +2,38 @@ package com.application.foodio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+//        navController = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment,
+            )
+        )
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(
+            navController
+        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
-    //Testing fist commit
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
